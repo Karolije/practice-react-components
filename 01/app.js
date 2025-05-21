d2799bab5134f492325e5bf3ac1dda3ad280e926
Karolina <karolina.jedrajczyk@gmail.com>
2025-05-21 09:17:02 +0200

    done task 01

--- 01/app.js
+++ 01/app.js
@@ -14,10 +14,18 @@ class App extends React.Component {
     return <h1>{this.state.counter}</h1>;
   }
   componentDidMount() {
+    console.log("componentDidMount");
     this.id = setInterval(() => {
       const { counter } = this.state;
       this.setState({ counter: counter + 1 });
     }, 5000);
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    clearInterval(this.id);
   }
 }
 
